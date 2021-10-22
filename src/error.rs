23b63a78529df889e5a22e5403d288776c6376a2
Sho Nakatani <lay.sakura@gmail.com>
2021-10-22 10:31:30 +0900

    feat: SpringResult

--- src/error.rs
+++ src/error.rs
@@ -4,10 +4,13 @@ use std::io;
 
 use thiserror::Error;
 
+/// Result type
+pub type Result<T> = std::result::Result<T, SpringError>;
+
 /// Error type
 #[allow(missing_docs)]
 #[derive(Debug, Error)]
-enum SpringError {
+pub enum SpringError {
     #[error("I/O error")]
     Io(#[from] io::Error),
 }
