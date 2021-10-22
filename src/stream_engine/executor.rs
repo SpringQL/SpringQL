6c61bb1769a0ca8eaf4d96cc9ef4e219cd50fb66
Sho Nakatani <lay.sakura@gmail.com>
2021-10-22 14:49:56 +0900

    test: fixture

--- src/stream_engine/executor.rs
+++ src/stream_engine/executor.rs
@@ -2,3 +2,6 @@ mod server;
 
 mod row;
 mod row_reader;
+
+#[cfg(test)]
+pub mod test_support;
---
+++ src/stream_engine/executor/test_support/fixture.rs
@@ -0,0 +1,15 @@
+use crate::stream_engine::executor::row::Row;
+
+impl Row {
+    pub fn fx_tokyo() -> Self {
+        todo!()
+    }
+
+    pub fn fx_osaka() -> Self {
+        todo!()
+    }
+
+    pub fn fx_london() -> Self {
+        todo!()
+    }
+}
