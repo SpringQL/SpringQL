7c6f38c1169b5b55c5f3c9fea2abb79eb24ce433
Sho Nakatani <lay.sakura@gmail.com>
2021-10-24 05:12:35 +0900

    refactor: JsonObject::new

--- src/stream_engine/executor/foreign_input_row/format/json.rs
+++ src/stream_engine/executor/foreign_input_row/format/json.rs
@@ -1,14 +1,8 @@
-#[derive(Clone, Eq, PartialEq, Debug)]
+#[derive(Clone, Eq, PartialEq, Debug, new)]
 pub(in crate::stream_engine) struct JsonObject(serde_json::Value);
 
 impl ToString for JsonObject {
     fn to_string(&self) -> String {
         self.0.to_string()
-    }
-}
-
-impl JsonObject {
-    pub(in crate::stream_engine) fn new(value: serde_json::Value) -> Self {
-        Self(value)
     }
 }
