ac0edff5cd06097b5a9314c6fbc4e1e4f55c9bb0
Sho Nakatani <lay.sakura@gmail.com>
2021-10-24 05:18:21 +0900

    feat: Row: Eq + Ord

--- src/stream_engine/executor/row.rs
+++ src/stream_engine/executor/row.rs
@@ -7,8 +7,21 @@ pub(in crate::stream_engine::executor) mod row_chunk;
 /// - Clone/Copy is disabled.
 /// - Immutable. Modification (adding / removing any column or updating column value) leads to new Row.
 /// - Mandatory `timestamp` column.
-#[derive(Debug)]
+/// - Ord by timestamp.
+/// - Eq by all columns.
+#[derive(Eq, PartialEq, Debug)]
 pub(super) struct Row {
     /// Timestamp
     ts: Timestamp,
+}
+
+impl Ord for Row {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.ts.cmp(&other.ts)
+    }
+}
+impl PartialOrd for Row {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
 }
