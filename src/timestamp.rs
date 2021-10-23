7d555f8a6c0943bb77c12a942e5126a1db2b96e9
Sho Nakatani <lay.sakura@gmail.com>
2021-10-24 06:05:47 +0900

    docs: less comment and add link

--- src/timestamp.rs
+++ src/timestamp.rs
@@ -34,19 +34,13 @@ impl ToString for Timestamp {
     }
 }
 
+/// See: <https://serde.rs/custom-date-format.html>
 mod datetime_format {
     use chrono::NaiveDateTime;
     use serde::{self, Deserialize, Deserializer, Serializer};
 
     use crate::timestamp::FORMAT;
 
-    // The signature of a serialize_with function must follow the pattern:
-    //
-    //    fn serialize<S>(&T, S) -> Result<S::Ok, S::Error>
-    //    where
-    //        S: Serializer
-    //
-    // although it may also be generic over the input types T.
     pub fn serialize<S>(date: &NaiveDateTime, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
@@ -55,13 +49,6 @@ mod datetime_format {
         serializer.serialize_str(&s)
     }
 
-    // The signature of a deserialize_with function must follow the pattern:
-    //
-    //    fn deserialize<'de, D>(D) -> Result<T, D::Error>
-    //    where
-    //        D: Deserializer<'de>
-    //
-    // although it may also be generic over the output types T.
     pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
     where
         D: Deserializer<'de>,
