0e0addb7ba9885d7b79b1968b26ccc37ea6d6333
Sho Nakatani <lay.sakura@gmail.com>
2021-10-24 10:03:35 +0900

    feat: RowRepository as trait

--- src/stream_engine/executor/data/row/repository.rs
+++ src/stream_engine/executor/data/row/repository.rs
@@ -1,1 +1,11 @@
-struct RowRepository;
+use crate::{error::Result, stream_engine::model::stream_model::StreamModel};
+
+use super::Row;
+
+pub(in crate::stream_engine::executor) trait RowRepository {
+    /// Get the next row from `source_stream`.
+    fn collect_next(&self, source_stream: &StreamModel) -> Result<&Row>;
+
+    /// Move `row` to `row.stream()`.
+    fn emit(&self, row: Row) -> Result<()>;
+}
