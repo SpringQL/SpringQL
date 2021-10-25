e9fbb188df7a7f267598f16d1312182071c1db72
Sho Nakatani <lay.sakura@gmail.com>
2021-10-25 17:30:15 +0900

    docs: Concept diagram of row repository

--- src/stream_engine/executor/data/row/repository.rs
+++ src/stream_engine/executor/data/row/repository.rs
@@ -1,4 +1,13 @@
-use crate::{error::Result, model::name::StreamName};
+#[cfg(test)]
+pub(crate) mod naive_row_repository;
+
+#[cfg(test)]
+pub(crate) use naive_row_repository::NaiveRowRepository;
+
+use crate::{
+    error::Result,
+    model::name::{PumpName, StreamName},
+};
 
 use super::Row;
 
@@ -11,14 +20,64 @@ impl RowRef {
     }
 }
 
+/// # Concept diagram
+///
+/// ```text
+/// ---> (Stream "s1") -+- (Pump "s1_p1") ---> (Stream "s2")
+///                     |   in buf: []
+///                     |
+///                     +- (Pump "s1_p2") ---> (Stream "s3")
+///                         in buf: []
+/// ```
+///
+/// ```text
+/// emit(r1, "s1");
+///
+/// ---> (Stream "s1") -+- (Pump "s1_p1") ---> (Stream "s2")
+///                     |   in buf: [r1]
+///                     |
+///                     +- (Pump "s1_p2") ---> (Stream "s3")
+///                         in buf: [r1]
+/// ```
+///
+/// ```text
+/// collect_next("s1_p1");  // -> r1
+///
+/// ---> (Stream "s1") -+- (Pump "s1_p1") ---> (Stream "s2")
+///                     |   in buf: []
+///                     |
+///                     +- (Pump "s1_p2") ---> (Stream "s3")
+///                         in buf: [r1]
+/// ```
+///
+/// ```text
+/// emit(r2, "s1");
+///
+/// ---> (Stream "s1") -+- (Pump "s1_p1") ---> (Stream "s2")
+///                     |   in buf: [r2]
+///                     |
+///                     +- (Pump "s1_p2") ---> (Stream "s3")
+///                         in buf: [r2, r1]
+/// ```
+///
+/// ```text
+/// collect_next("s1_p2");  // -> r1
+///
+/// ---> (Stream "s1") -+- (Pump "s1_p1") ---> (Stream "s2")
+///                     |   in buf: [r2]
+///                     |
+///                     +- (Pump "s1_p2") ---> (Stream "s3")
+///                         in buf: [r2]
+/// ```
 pub(crate) trait RowRepository {
     /// Get ref to Row from RowRef.
     fn get(&self, row_ref: &RowRef) -> &Row;
 
-    /// Get the next RowRef from `source_stream`.
-    fn collect_next(&self, source_stream: &StreamName) -> Result<RowRef>;
+    /// Get the next RowRef from `pump`.
+    fn collect_next(&self, pump: &PumpName) -> Result<RowRef>;
 
     /// Move `row` to `dest_stream`.
+    /// The row is distributed to all pumps going out from `dest_stream`.
     fn emit(&self, row: Row, dest_stream: &StreamName) -> Result<()>;
 }
 
@@ -30,7 +89,7 @@ impl RowRepository for RefCntGcRowRepository {
         todo!()
     }
 
-    fn collect_next(&self, source_stream: &StreamName) -> Result<RowRef> {
+    fn collect_next(&self, pump: &PumpName) -> Result<RowRef> {
         todo!()
     }
 
