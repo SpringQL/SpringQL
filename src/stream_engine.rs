5b8707611f5a62691a76c40a82059feb35227681
Sho Nakatani <lay.sakura@gmail.com>
2021-10-29 16:08:41 +0900

    feat: StreamEngine

--- src/stream_engine.rs
+++ src/stream_engine.rs
@@ -1,6 +1,17 @@
 //! Stream Engine component.
 //!
 //! Responsible for pipeline management and execution.
+//!
+//! Stream engine has 2 executors:
+//!
+//! 1. Reactive executor
+//! 2. Autonomous executor
+//!
+//! Reactive executor quickly changes some status of a pipeline. It does not deal with stream data (Row, to be precise).
+//! Autonomous executor deals with stream data.
+//!
+//! Reactive executor resides in a main thread, while autonomous executor works with separate threads (models as WorkerPool).
+//! ```
 
 mod executor;
 
@@ -8,3 +19,6 @@ pub(crate) use executor::{CurrentTimestamp, RowRepository, Timestamp};
 
 #[cfg(test)]
 pub(crate) use executor::TestRowRepository;
+
+#[derive(Debug)]
+pub(crate) struct StreamEngine;
