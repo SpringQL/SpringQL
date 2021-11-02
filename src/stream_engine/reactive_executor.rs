fe4cf1421fdf63272246b72623f2538a6f64b0b7
Sho Nakatani <lay.sakura@gmail.com>
2021-11-02 12:42:54 +0900

    wip

--- src/stream_engine/reactive_executor.rs
+++ src/stream_engine/reactive_executor.rs
@@ -20,8 +20,20 @@ impl ReactiveExecutor {
         &mut self,
         command: AlterPipelineCommand,
     ) -> Result<Arc<Pipeline>> {
-        let new_pipeline = todo!();
+        let new_pipeline = Self::_alter_pipeline(self.pipeline.clone(), command)?;
         self.pipeline = Arc::new(new_pipeline);
         Ok(self.pipeline.clone())
+    }
+
+    fn _alter_pipeline(pipeline: Arc<Pipeline>, command: AlterPipelineCommand) -> Result<Pipeline> {
+        match command {
+            AlterPipelineCommand::CreateStream(_) => todo!(),
+            AlterPipelineCommand::CreateForeignStream(server) => Self::create_foreign_stream(
+                pipeline,
+                server,
+            ),
+            AlterPipelineCommand::CreatePump(_) => todo!(),
+            AlterPipelineCommand::AlterPump(_) => todo!(),
+        }
     }
 }
