8ac25763d7725946a6797e54250fb9cb148ee7f6
Sho Nakatani <lay.sakura@gmail.com>
2021-11-04 23:12:13 +0900

    test: test_e2e_source_sink

--- springql-core/tests/e2e_row_level_rs.rs
+++ springql-core/tests/e2e_row_level_rs.rs
@@ -1,60 +1,95 @@
-use regex::Regex;
+use serde_json::json;
 use springql_core::error::Result;
 use springql_core::low_level_rs::*;
+use test_foreign_service::sink::TestForeignSink;
+use test_foreign_service::source::TestForeignSource;
 
-#[ignore]
+fn apply_ddls(ddls: &[String]) -> SpringPipeline {
+    let pipeline = spring_open().unwrap();
+    for ddl in ddls {
+        let stmt = spring_prepare(&pipeline, &ddl).unwrap();
+        assert_eq!(spring_step(&stmt).unwrap(), SpringStepSuccess::Done);
+    }
+    pipeline
+}
+
+fn drain_from_sink(sink: &TestForeignSink) -> Vec<serde_json::Value> {
+    let mut received = Vec::new();
+    while let Ok(v) = sink.receive() {
+        received.push(v);
+    }
+    received
+}
+
 #[test]
 fn test_e2e_source_sink() -> Result<()> {
-    let pipeline = spring_open()?;
+    let json_oracle = json!({
+        "ts": "2021-11-04 23:02:52.123456789",
+        "ticker": "ORCL",
+        "amount": 20,
+    });
+    let json_ibm = json!({
+        "ts": "2021-11-04 23:03:29.123456789",
+        "ticker": "IBM",
+        "amount": 30,
+    });
+    let json_google = json!({
+        "ts": "2021-11-04 23:03:42.123456789",
+        "ticker": "GOOGL",
+        "amount": 100,
+    });
+    let source_input = vec![json_oracle, json_ibm, json_google];
+
+    let test_source = TestForeignSource::start(source_input.clone()).unwrap();
+    let test_sink = TestForeignSink::start().unwrap();
 
     let ddls = vec![
-        "
-        CREATE FOREIGN STREAM fst_trade_input (
-          timestamp TIMESTAMP NOT NULL ROWTIME,    
+        format!(
+            "
+        CREATE SOURCE STREAM source_trade (
+          ts TIMESTAMP NOT NULL ROWTIME,    
           ticker TEXT NOT NULL,
           amount INTEGER NOT NULL
         ) SERVER NET_SERVER OPTIONS (
-          REMOTE_PORT '17890'
+          PROTOCOL 'TCP',
+          REMOTE_HOST '{remote_host}',
+          REMOTE_PORT '{remote_port}'
         );
         ",
-        "
-        CREATE FOREIGN STREAM fst_trade_output (
-          timestamp TIMESTAMP NOT NULL,    
+            remote_host = test_source.host_ip(),
+            remote_port = test_source.port()
+        ),
+        format!(
+            "
+        CREATE SINK STREAM sink_trade (
+          ts TIMESTAMP NOT NULL,    
           ticker TEXT NOT NULL,
           amount INTEGER NOT NULL
-        ) SERVER IN_MEMORY_QUEUE;
+        ) SERVER NET_SERVER OPTIONS (
+          PROTOCOL 'TCP',
+          REMOTE_HOST '{remote_host}',
+          REMOTE_PORT '{remote_port}'
+        );
         ",
+            remote_host = test_source.host_ip(),
+            remote_port = test_source.port()
+        ),
         "
-        CREATE PUMP pu_in_to_out AS
-          INSERT INTO fst_trade_output (timestamp, ticker, amount)
-          SELECT STREAM timestamp, ticker, amount FROM fst_trade_input;
-        ",
+        CREATE PUMP pu_passthrough AS
+          INSERT INTO sink_trade (ts, ticker, amount)
+          SELECT STREAM ts, ticker, amount FROM source_trade;
+        "
+        .to_string(),
+        "
+        ALTER PUMP pu_passthrough START;
+        "
+        .to_string(),
     ];
-    for ddl in ddls {
-        let stmt = spring_prepare(&pipeline, ddl)?;
-        assert_eq!(spring_step(&stmt)?, SpringStepSuccess::Done);
-    }
 
-    let stmt = spring_prepare(
-        &pipeline,
-        "SELECT timestamp, ticker, amount FROM fst_trade_output;",
-    )?;
+    let _pipeline = apply_ddls(&ddls);
+    let sink_received = drain_from_sink(&test_sink);
 
-    let re_timestamp = Regex::new(r"^\d{4}-\d{2}-\d{2} \d{2}-\d{2}-\d{2}\.\d{9}").unwrap();
-    let re_ticker = Regex::new(r"^[A-Z0-9]{2,5}").unwrap();
-
-    for _ in 0..1000 {
-        assert_eq!(spring_step(&stmt)?, SpringStepSuccess::Row);
-
-        let timestamp = spring_column_text(&stmt, 0)?;
-        assert!(re_timestamp.is_match(&timestamp));
-
-        let ticker = spring_column_text(&stmt, 1)?;
-        assert!(re_ticker.is_match(&ticker));
-
-        let amount = spring_column_i32(&stmt, 2)?;
-        assert!(amount > 1);
-    }
+    assert_eq!(source_input, sink_received);
 
     Ok(())
 }
