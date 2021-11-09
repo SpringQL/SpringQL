e7e0a01b4c29acfe81c5e7233d7b1044bf3ef057
Sho Nakatani <lay.sakura@gmail.com>
2021-11-09 09:52:30 +0900

    test: test_spring_open_twice

--- springql-core/tests/feat_spring_open_twice.rs
+++ springql-core/tests/feat_spring_open_twice.rs
@@ -1,11 +1,7 @@
-use springql_core::{error::SpringError, low_level_rs::spring_open};
+use springql_core::low_level_rs::spring_open;
 
 #[test]
-fn test_spring_open_twice_unavailable() {
+fn test_spring_open_twice() {
     let _ = spring_open().unwrap();
-
-    assert!(matches!(
-        spring_open().unwrap_err(),
-        SpringError::Unavailable { .. }
-    ))
+    let _ = spring_open().unwrap();
 }
