3fd97c4ec8f67a6e5fc696ee96077d876dd494ac
Sho Nakatani <lay.sakura@gmail.com>
2021-11-22 09:17:57 +0900

    refactor: rm unused struct

--- springql-core/src/api/low_level_rs.rs
+++ springql-core/src/api/low_level_rs.rs
@@ -36,27 +36,6 @@ impl From<ForeignSinkRow> for SpringRow {
     }
 }
 
-/// Successful response from `spring_step()`.
-#[derive(Eq, PartialEq, Debug)]
-pub enum SpringStepSuccess {
-    /// No more rows available.
-    ///
-    /// You must not call `spring_step()` again with the same prepared statement object
-    /// after you get this response.
-    ///
-    /// Returned on:
-    /// - DDL statements
-    /// - SELECT statements to table after reading final row.
-    Done,
-
-    /// Your prepared statement has gotten a new row.
-    /// You can call `spring_column_*()` to get column values.
-    ///
-    /// Returned on:
-    /// - SELECT statements to stream or table.
-    Row,
-}
-
 /// Creates and open an in-process stream pipeline.
 pub fn spring_open() -> Result<SpringPipeline> {
     let engine = EngineMutex::new(N_WORKER_THREADS);
