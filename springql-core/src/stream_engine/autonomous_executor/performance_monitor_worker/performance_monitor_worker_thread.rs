1050921795da12a8da240466b41725e4dc56b494
Sho Nakatani <lay.sakura@gmail.com>
2022-01-08 15:54:11 +0900

    refactor: impl WorkerThread for PerformanceMonitorWorkerThread

--- springql-core/src/stream_engine/autonomous_executor/performance_monitor_worker/performance_monitor_worker_thread.rs
+++ springql-core/src/stream_engine/autonomous_executor/performance_monitor_worker/performance_monitor_worker_thread.rs
@@ -1,100 +1,68 @@
-use std::{
-    sync::{mpsc, Arc},
-    thread,
-    time::Duration,
+use std::{sync::Arc, thread, time::Duration};
+
+use crate::stream_engine::autonomous_executor::{
+    event_queue::event::EventTag, performance_metrics::PerformanceMetrics,
+    pipeline_derivatives::PipelineDerivatives, worker::WorkerThread,
 };
 
-use crate::{
-    pipeline::Pipeline,
-    stream_engine::{
-        autonomous_executor::{
-            event_queue::{
-                event::{Event, EventTag},
-                EventPoll, EventQueue,
-            },
-            performance_metrics::PerformanceMetrics,
-            pipeline_derivatives::PipelineDerivatives,
-        },
-        time::duration::wall_clock_duration::WallClockDuration,
-    },
+use super::{
+    web_console_reporter::WebConsoleReporter, CLOCK_MSEC, REPORT_INTERVAL_CLOCK_WEB_CONSOLE,
 };
-
-use super::web_console_reporter::WebConsoleReporter;
-
-// TODO config
-const CLOCK_MSEC: u64 = 100;
-const REPORT_INTERVAL_CLOCK_WEB_CONSOLE: u64 = 30;
-const WEB_CONSOLE_HOST: &str = "127.0.0.1";
-const WEB_CONSOLE_PORT: u16 = 8050;
-const WEB_CONSOLE_TIMEOUT: WallClockDuration =
-    WallClockDuration::from_millis(CLOCK_MSEC * REPORT_INTERVAL_CLOCK_WEB_CONSOLE);
 
 /// Runs a worker thread.
 #[derive(Debug)]
 pub(super) struct PerformanceMonitorWorkerThread;
 
-impl PerformanceMonitorWorkerThread {
-    pub(super) fn run(event_queue: Arc<EventQueue>, stop_receiver: mpsc::Receiver<()>) {
-        let event_poll_update_pipeline = event_queue.subscribe(EventTag::UpdatePipeline);
+#[derive(Debug, new)]
+pub(super) struct PerformanceMonitorWorkerThreadArg {
+    web_console_reporter: WebConsoleReporter,
+}
 
-        let web_console_reporter =
-            WebConsoleReporter::new(WEB_CONSOLE_HOST, WEB_CONSOLE_PORT, WEB_CONSOLE_TIMEOUT);
+#[derive(Debug, Default)]
+pub(super) struct PerformanceMonitorWorkerLoopState {
+    pipeline_derivatives: Arc<PipelineDerivatives>,
+    metrics: PerformanceMetrics,
+    clk_web_console: u64,
+}
 
-        let _ = thread::spawn(move || {
-            Self::main_loop(
-                web_console_reporter,
-                event_poll_update_pipeline,
-                stop_receiver,
-            )
-        });
+impl WorkerThread for PerformanceMonitorWorkerThread {
+    type ThreadArg = PerformanceMonitorWorkerThreadArg;
+
+    type LoopState = PerformanceMonitorWorkerLoopState;
+
+    fn event_subscription() -> Vec<EventTag> {
+        vec![EventTag::UpdatePipeline]
     }
 
-    fn main_loop(
-        web_console_reporter: WebConsoleReporter,
-        event_poll_update_pipeline: EventPoll,
-        stop_receiver: mpsc::Receiver<()>,
-    ) {
-        let mut pipeline_derivatives = Arc::new(PipelineDerivatives::new(Pipeline::default()));
-        let mut metrics = PerformanceMetrics::default();
-        let mut clk_wc = REPORT_INTERVAL_CLOCK_WEB_CONSOLE;
+    fn main_loop_cycle(
+        current_state: Self::LoopState,
+        thread_arg: &Self::ThreadArg,
+    ) -> Self::LoopState {
+        let mut state = current_state;
 
-        log::debug!("[PerformanceMonitorWorker] Started");
-
-        while stop_receiver.try_recv().is_err() {
-            pipeline_derivatives = Self::handle_interruption(
-                &event_poll_update_pipeline,
-                pipeline_derivatives,
-                &mut metrics,
-            );
-
-            if clk_wc == 0 {
-                clk_wc = REPORT_INTERVAL_CLOCK_WEB_CONSOLE;
-                web_console_reporter.report(&metrics, pipeline_derivatives.task_graph());
-            }
-
-            thread::sleep(Duration::from_millis(CLOCK_MSEC))
+        if state.clk_web_console == 0 {
+            state.clk_web_console = REPORT_INTERVAL_CLOCK_WEB_CONSOLE;
+            thread_arg
+                .web_console_reporter
+                .report(&state.metrics, state.pipeline_derivatives.task_graph());
         }
+        thread::sleep(Duration::from_millis(CLOCK_MSEC));
+
+        state
     }
 
-    /// May re-create PipelineDerivatives and PerformanceMetrics.
-    ///
-    /// # Returns
-    ///
-    /// Some on interruption.
-    fn handle_interruption(
-        event_poll_update_pipeline: &EventPoll,
+    fn ev_update_pipeline(
+        current_state: Self::LoopState,
         pipeline_derivatives: Arc<PipelineDerivatives>,
-        metrics: &mut PerformanceMetrics,
-    ) -> Arc<PipelineDerivatives> {
-        if let Some(Event::UpdatePipeline {
-            pipeline_derivatives,
-        }) = event_poll_update_pipeline.poll()
-        {
-            log::debug!("[PerformanceMonitorWorker] got UpdatePipeline event");
-            metrics.reset_from_task_graph(pipeline_derivatives.task_graph());
-            pipeline_derivatives
-        } else {
-            pipeline_derivatives
-        }
+        _thread_arg: &Self::ThreadArg,
+    ) -> Self::LoopState {
+        let mut state = current_state;
+
+        state
+            .metrics
+            .reset_from_task_graph(pipeline_derivatives.task_graph());
+        state.pipeline_derivatives = pipeline_derivatives;
+
+        state
     }
 }
