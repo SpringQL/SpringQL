049b633fd235e588e4affaf09a7be7c62ab7fe82
Sho Nakatani <lay.sakura@gmail.com>
2022-01-27 10:40:20 +0900

    chore: Timestamp::{ceil,floor}

--- springql-core/src/stream_engine/time/timestamp.rs
+++ springql-core/src/stream_engine/time/timestamp.rs
@@ -32,6 +32,35 @@ impl MemSize for Timestamp {
     }
 }
 
+impl Timestamp {
+    /// Note: `2262-04-11T23:47:16.854775804` is the maximum possible timestamp because it uses nano-sec unixtime internally.
+    pub(crate) fn floor(&self, resolution: Duration) -> Timestamp {
+        let ts_nano = self.0.timestamp_nanos();
+        let resolution_nano = resolution.num_nanoseconds().expect("no overflow");
+        assert!(resolution_nano > 0);
+
+        let floor_ts_nano = (ts_nano / resolution_nano) * resolution_nano;
+
+        let floor_naive_date_time = {
+            let floor_ts_secs = floor_ts_nano / 1_000_000_000;
+            let floor_ts_nanos = floor_ts_nano % 1_000_000_000;
+            NaiveDateTime::from_timestamp(floor_ts_secs, floor_ts_nanos as u32)
+        };
+
+        Timestamp(floor_naive_date_time)
+    }
+
+    /// Note: `2262-04-11T23:47:16.854775804` is the maximum possible timestamp because it uses nano-sec unixtime internally.
+    pub(crate) fn ceil(&self, resolution: Duration) -> Timestamp {
+        let floor = self.floor(resolution);
+        if &floor == self {
+            floor
+        } else {
+            floor + resolution
+        }
+    }
+}
+
 impl FromStr for Timestamp {
     type Err = SpringError;
 
@@ -103,6 +132,198 @@ mod datetime_format {
 mod tests {
     use super::*;
     use crate::error::Result;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_floor() {
+        fn t(ts: &str, resolution: Duration, expected: &str) {
+            let ts = Timestamp::from_str(ts).unwrap();
+            let expected = Timestamp::from_str(expected).unwrap();
+
+            let actual = ts.floor(resolution);
+            assert_eq!(actual, expected);
+        }
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::seconds(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 23:59:59.999999999",
+            Duration::seconds(1),
+            "2020-01-01 23:59:59.000000000",
+        );
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::minutes(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 23:59:59.999999999",
+            Duration::minutes(1),
+            "2020-01-01 23:59:00.000000000",
+        );
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::hours(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 23:59:59.999999999",
+            Duration::hours(1),
+            "2020-01-01 23:00:00.000000000",
+        );
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::days(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 23:59:59.999999999",
+            Duration::days(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::milliseconds(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 23:59:59.999999999",
+            Duration::milliseconds(1),
+            "2020-01-01 23:59:59.999000000",
+        );
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::microseconds(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 23:59:59.999999999",
+            Duration::microseconds(1),
+            "2020-01-01 23:59:59.999999000",
+        );
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::nanoseconds(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 23:59:59.999999999",
+            Duration::nanoseconds(1),
+            "2020-01-01 23:59:59.999999999",
+        );
+        t(
+            "2020-01-01 23:59:59.999999999",
+            Duration::nanoseconds(10),
+            "2020-01-01 23:59:59.999999990",
+        );
+    }
+
+    #[test]
+    fn test_ceil() {
+        fn t(ts: &str, resolution: Duration, expected: &str) {
+            let ts = Timestamp::from_str(ts).unwrap();
+            let expected = Timestamp::from_str(expected).unwrap();
+
+            let actual = ts.ceil(resolution);
+            assert_eq!(actual, expected);
+        }
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::seconds(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 00:00:00.000000001",
+            Duration::seconds(1),
+            "2020-01-01 00:00:01.000000000",
+        );
+        t(
+            "2020-01-01 23:59:59.999999999",
+            Duration::seconds(1),
+            "2020-01-02 00:00:00.000000000",
+        );
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::minutes(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 00:00:00.000000001",
+            Duration::minutes(1),
+            "2020-01-01 00:01:00.000000000",
+        );
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::hours(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 00:00:00.000000001",
+            Duration::hours(1),
+            "2020-01-01 01:00:00.000000000",
+        );
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::days(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 00:00:00.000000001",
+            Duration::days(1),
+            "2020-01-02 00:00:00.000000000",
+        );
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::milliseconds(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 00:00:00.000000001",
+            Duration::milliseconds(1),
+            "2020-01-01 00:00:00.001000000",
+        );
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::microseconds(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 00:00:00.000000001",
+            Duration::microseconds(1),
+            "2020-01-01 00:00:00.000001000",
+        );
+
+        t(
+            "2020-01-01 00:00:00.000000000",
+            Duration::nanoseconds(1),
+            "2020-01-01 00:00:00.000000000",
+        );
+        t(
+            "2020-01-01 00:00:00.000000001",
+            Duration::nanoseconds(1),
+            "2020-01-01 00:00:00.000000001",
+        );
+        t(
+            "2020-01-01 00:00:00.000000001",
+            Duration::nanoseconds(10),
+            "2020-01-01 00:00:00.000000010",
+        );
+    }
 
     #[test]
     fn test_timestamp_ser_de() -> Result<()> {
