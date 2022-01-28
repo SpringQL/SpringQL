6bb74e9627c2ef5b234023dfc1bb5a458b2db07c
Sho Nakatani <lay.sakura@gmail.com>
2022-01-28 12:53:48 +0900

    chore: wrap by Mutex

--- springql-core/src/stream_engine/autonomous_executor/task/pump_task/pump_subtask/query_subtask/query_subtask_node/group_aggregate_window_subtask.rs
+++ springql-core/src/stream_engine/autonomous_executor/task/pump_task/pump_subtask/query_subtask/query_subtask_node/group_aggregate_window_subtask.rs
@@ -1,12 +1,14 @@
 // Copyright (c) 2021 TOYOTA MOTOR CORPORATION. Licensed under MIT OR Apache-2.0.
 
+use std::sync::Mutex;
+
 use crate::pipeline::pump_model::window_operation_parameter::WindowOperationParameter;
 use crate::pipeline::pump_model::window_parameter::WindowParameter;
 use crate::stream_engine::autonomous_executor::task::tuple::Tuple;
 use crate::stream_engine::autonomous_executor::task::window::Window;
 
 #[derive(Debug)]
-pub(in crate::stream_engine::autonomous_executor) struct GroupAggregateWindowSubtask(Window);
+pub(in crate::stream_engine::autonomous_executor) struct GroupAggregateWindowSubtask(Mutex<Window>);
 
 impl GroupAggregateWindowSubtask {
     pub(in crate::stream_engine::autonomous_executor) fn new(
@@ -14,10 +16,13 @@ impl GroupAggregateWindowSubtask {
         op_param: WindowOperationParameter,
     ) -> Self {
         let window = Window::new(window_param, op_param);
-        Self(window)
+        Self(Mutex::new(window))
     }
 
     pub(in crate::stream_engine::autonomous_executor) fn run(&self, tuple: Tuple) -> Vec<Tuple> {
-        self.0.dispatch(tuple)
+        self.0
+            .lock()
+            .expect("another thread accessing to window gets poisoned")
+            .dispatch(tuple)
     }
 }
