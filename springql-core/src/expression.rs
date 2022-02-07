04a17a2138cb99c34bb70ddd440235806efd2cfa
Sho Nakatani <lay.sakura@gmail.com>
2022-02-07 12:07:45 +0900

    docs: expression types

--- springql-core/src/expression.rs
+++ springql-core/src/expression.rs
@@ -1,3 +1,11 @@
+//! Expression has two forms:
+//!
+//! 1. Value expression, which is evaluated into an SqlValue from a row.
+//! 2. Aggregate expression, which is evaluated into an SqlValue from set of rows.
+//!
+//! Since SQL parser cannot distinguish column reference and value expression,
+//! `ValueExprOrAlias` is used for value expressions excluding select_list.
+
 pub(crate) mod boolean_expression;
 pub(crate) mod function_call;
 pub(crate) mod operator;
