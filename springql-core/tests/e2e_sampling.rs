3ca9292bb623c055725f84b057e3299ed3d3b6e2
Sho Nakatani <lay.sakura@gmail.com>
2022-02-10 16:24:01 +0900

    test: add test to checko for pumps to insert into window queues

--- springql-core/tests/e2e_sampling.rs
+++ springql-core/tests/e2e_sampling.rs
@@ -129,3 +129,101 @@ fn test_e2e_sampling() -> Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn test_e2e_sampling_pump_insert_to_window_queue() -> Result<()> {
+    setup_test_logger();
+
+    let source_input = gen_source_input();
+
+    let test_source =
+        ForeignSource::start(ForeignSourceInput::new_fifo_batch(source_input)).unwrap();
+    let test_sink = ForeignSink::start().unwrap();
+
+    let ddls = vec![
+        "
+        CREATE SOURCE STREAM source_trade (
+          ts TIMESTAMP NOT NULL ROWTIME,    
+          ticker TEXT NOT NULL,
+          amount INTEGER NOT NULL
+        );
+        "
+        .to_string(),
+        "
+        CREATE STREAM trade_amount (
+          ts TIMESTAMP NOT NULL ROWTIME,    
+          amount INTEGER NOT NULL
+        );
+        "
+        .to_string(),
+        "
+        CREATE SINK STREAM sink_sampled_trade_amount (
+          ts TIMESTAMP NOT NULL ROWTIME,    
+          amount INTEGER NOT NULL
+        );
+        "
+        .to_string(),
+        "
+        CREATE PUMP pu_projection AS
+          INSERT INTO trade_amount (ts, amount)
+          SELECT STREAM
+            source_trade.ts,
+            source_trade.amount
+          FROM source_trade;
+        "
+        .to_string(),
+        "
+        CREATE PUMP pu_sampling AS
+          INSERT INTO sink_sampled_trade_amount (ts, amount)
+          SELECT STREAM
+            FLOOR_TIME(trade_amount.ts, DURATION_SECS(10)) AS sampled_ts,
+            AVG(trade_amount.amount) AS avg_amount
+          FROM trade_amount
+          GROUP BY sampled_ts
+          FIXED WINDOW DURATION_SECS(10), DURATION_SECS(0);
+        "
+        .to_string(),
+        format!(
+            "
+        CREATE SINK WRITER tcp_sink_trade FOR sink_sampled_trade_amount
+          TYPE NET_SERVER OPTIONS (
+            PROTOCOL 'TCP',
+            REMOTE_HOST '{remote_host}',
+            REMOTE_PORT '{remote_port}'
+        );
+        ",
+            remote_host = test_sink.host_ip(),
+            remote_port = test_sink.port()
+        ),
+        format!(
+            "
+        CREATE SOURCE READER tcp_source_trade FOR source_trade
+          TYPE NET_SERVER OPTIONS (
+            PROTOCOL 'TCP',
+            REMOTE_HOST '{remote_host}',
+            REMOTE_PORT '{remote_port}'
+          );
+        ",
+            remote_host = test_source.host_ip(),
+            remote_port = test_source.port()
+        ),
+    ];
+
+    let sink_received = run_and_drain(&ddls, &test_sink);
+
+    assert_eq!(sink_received.len(), 2);
+
+    assert_eq!(
+        sink_received[0]["ts"].as_str().unwrap(),
+        "2020-01-01 00:00:00.000000000"
+    );
+    assert_eq!(sink_received[0]["amount"].as_i64().unwrap(), 20,);
+
+    assert_eq!(
+        sink_received[1]["ts"].as_str().unwrap(),
+        "2020-01-01 00:00:10.000000000"
+    );
+    assert_eq!(sink_received[1]["amount"].as_i64().unwrap(), 50,);
+
+    Ok(())
+}
