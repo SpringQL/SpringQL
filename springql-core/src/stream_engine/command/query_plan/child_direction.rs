34f70d5867204b75000bcbdc6fb8ddfc545a1821
Sho Nakatani <lay.sakura@gmail.com>
2022-04-08 14:56:05 +0900

    chore: remove unused mod

--- springql-core/src/stream_engine/command/query_plan/child_direction.rs
+++
@@ -1,9 +0,0 @@
-// This file is part of https://github.com/SpringQL/SpringQL which is licensed under MIT OR Apache-2.0. See file LICENSE-MIT or LICENSE-APACHE for full license details.
-
-use serde::{Deserialize, Serialize};
-
-#[derive(Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
-pub(in crate::stream_engine) enum ChildDirection {
-    Left,
-    Right,
-}
