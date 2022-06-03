7e2fbdd23a3d4f8abf5bf3e83c3a80e3af9322ec
Kazuhiko Kikuchi <kazuk.dll@kazuk.jp>
2022-06-04 08:28:22 +0900

    rewrite test_spring_open_twice

--- springql-core/tests/feat_spring_open_twice.rs
+++ springql-core/tests/feat_spring_open_twice.rs
@@ -1,12 +1,10 @@
 // This file is part of https://github.com/SpringQL/SpringQL which is licensed under MIT OR Apache-2.0. See file LICENSE-MIT or LICENSE-APACHE for full license details.
 
-use springql_core::api::spring_config_default;
+use springql_core::api::{spring_config_default, SpringPipelineHL};
 
 #[test]
-#[ignore = "have todo"]
 fn test_spring_open_twice() {
-    let _config = spring_config_default();
-    todo!("rewrite by high level API");
-    // let _ = spring_open(&config).unwrap();
-    // let _ = spring_open(&config).unwrap();
+    let config = spring_config_default();
+    SpringPipelineHL::new(&config).unwrap();
+    SpringPipelineHL::new(&config).unwrap();
 }
