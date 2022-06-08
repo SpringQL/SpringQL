49591a6f4da7cbfa6ed66f57157cacc171973ffe
Kazuhiko Kikuchi <kazuk.dll@kazuk.jp>
2022-06-08 09:38:22 +0900

    remove mod high_level_rs/low_level_rs
    move high_level_rs members to api
    move low_level_rs::spring_config mod to api
    remove fn spring_config_toml

--- springql-core/src/api/low_level_rs.rs
+++
@@ -1,9 +0,0 @@
-// This file is part of https://github.com/SpringQL/SpringQL which is licensed under MIT OR Apache-2.0. See file LICENSE-MIT or LICENSE-APACHE for full license details.
-
-//! Low-level API functions to execute / register SpringQL from Rust.
-//!
-//! C API and high-level Rust API are provided separately.
-
-pub mod spring_config;
-
-pub use crate::api::low_level_rs::spring_config::*;
