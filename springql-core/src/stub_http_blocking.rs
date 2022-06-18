d8babdc6a62351852f779e13bce6d93cf4687f4b
Kazuhiko Kikuchi <kazuk.dll@kazuk.jp>
2022-06-18 11:30:06 +0900

    fix copyright

--- springql-core/src/stub_http_blocking.rs
+++ springql-core/src/stub_http_blocking.rs
@@ -1,3 +1,5 @@
+// This file is part of https://github.com/SpringQL/SpringQL which is licensed under MIT OR Apache-2.0. See file LICENSE-MIT or LICENSE-APACHE for full license details.
+
 use std::{sync::RwLock, time::Duration};
 
 use once_cell::sync::OnceCell;
