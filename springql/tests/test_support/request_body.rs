87f051805dc01032d489de94634f26616f68210d
Kazuhiko Kikuchi <kazuk.dll@kazuk.jp>
2022-06-21 09:03:33 +0900

    use serde::Deserialize

--- springql/tests/test_support/request_body.rs
+++ springql/tests/test_support/request_body.rs
@@ -1,6 +1,6 @@
 // This file is part of https://github.com/SpringQL/SpringQL which is licensed under MIT OR Apache-2.0. See file LICENSE-MIT or LICENSE-APACHE for full license details.
 
-use serde_derive::Deserialize;
+use serde::Deserialize;
 
 #[derive(Debug, Clone, PartialEq, Deserialize)]
 pub struct PostTaskGraphBody {
