ca5bf55f5be132b7f50c0af2f06f17e7d9acd5bc
Kazuhiko Kikuchi <kazuk.dll@kazuk.jp>
2022-06-29 07:13:19 +0900

    add license line

--- springql/src/config.rs
+++ springql/src/config.rs
@@ -1,3 +1,5 @@
+// This file is part of https://github.com/SpringQL/SpringQL which is licensed under MIT OR Apache-2.0. See file LICENSE-MIT or LICENSE-APACHE for full license details.
+
 use serde::Deserialize;
 use springql_core::api::{
     Result, SpringConfig, SpringError, SpringMemoryConfig, SpringSinkWriterConfig,
