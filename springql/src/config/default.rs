c13cc17d95450ed6c46aa120a864f91a73aaaa1f
Kazuhiko Kikuchi <kazuk.dll@kazuk.jp>
2022-07-09 09:48:40 +0900

    add license line

--- springql/src/config/default.rs
+++ springql/src/config/default.rs
@@ -1,3 +1,5 @@
+// This file is part of https://github.com/SpringQL/SpringQL which is licensed under MIT OR Apache-2.0. See file LICENSE-MIT or LICENSE-APACHE for full license details.
+
 /// Default configuration.
 ///
 /// Default key-values are overwritten by `overwrite_config_toml` parameter in `SpringConfig::new()`.
