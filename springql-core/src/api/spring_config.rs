513bdc12af7a6b7e0e3f97b7d398da09bdc8a571
Kazuhiko Kikuchi <kazuk.dll@kazuk.jp>
2022-07-16 08:28:48 +0900

    move default config to pure file

--- springql-core/src/api/spring_config.rs
+++ springql-core/src/api/spring_config.rs
@@ -7,66 +7,7 @@ use crate::api::error::{Result, SpringError};
 /// Default configuration.
 ///
 /// Default key-values are overwritten by `overwrite_config_toml` parameter in `SpringConfig::new()`.
-const SPRING_CONFIG_DEFAULT: &str = r#"
-[worker]
-# Number of generic worker threads. Generic worker threads deal with internal and sink tasks.
-# Setting this to > 1 may improve throughput but lead to out-of-order stream processing.
-n_generic_worker_threads = 1
-
-# Number of source worker threads. Source worker threads collect rows from foreign source.
-# Too many number may may cause row fraud in runtime.
-# Setting this to > 1 may improve throughput but lead to out-of-order stream processing.
-n_source_worker_threads = 1
-
-# How long a generic worker or a source worker sleeps if it does not receive any row from the upstream.
-# Small number will improve the initial row's E2E latency but increase the CPU usage.
-sleep_msec_no_row = 100
-
-[memory]
-# How much memory is allowed to be used in SpringQL streaming runtime.
-upper_limit_bytes = 10_000_000
-
-# Percentage over `upper_limit_bytes` to transit from Moderate state to Severe.
-# In Severe state, internal scheduler is changed to exhibit memory-resilience.
-moderate_to_severe_percent = 60
-
-# Percentage over `upper_limit_bytes` to transit from Severe state to Critical.
-# In Critical state, all intermediate rows are purged to release memory.
-severe_to_critical_percent = 95
-
-critical_to_severe_percent = 80
-severe_to_moderate_percent = 40
-
-# Interval for MemoryStateMachineWorker to publish TransitPerformanceMetricsSummary event.
-memory_state_transition_interval_msec = 10
-
-# Interval for PerformanceMonitorWorker to publish ReportMetricsSummary event.
-performance_metrics_summary_report_interval_msec = 10
-
-[web_console]
-# Whether to enable POST API request to web console.
-enable_report_post = false
-
-report_interval_msec = 3_000
-
-host = "127.0.0.1"
-port = 8050
-
-timeout_msec = 3_000
-
-[source_reader]
-net_connect_timeout_msec = 1_000
-net_read_timeout_msec = 100
-
-can_read_timeout_msec = 100
-
-[sink_writer]
-net_connect_timeout_msec = 1_000
-net_write_timeout_msec = 100
-
-http_connect_timeout_msec = 1_000
-http_timeout_msec = 100
-"#;
+const SPRING_CONFIG_DEFAULT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/default_config.toml"));
 
 /// Top-level config.
 #[allow(missing_docs)]
